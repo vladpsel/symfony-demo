724e6f49336fa1a5a3d4e3c053f7612d3ad7ee3a
vladpsel <vpanovwr@gmail.com>
2022-01-23 17:52:32 +0200

    update(index): setup index page controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController
+{
+
+    public function home(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+}
